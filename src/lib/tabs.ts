f1b51d93a806d7bb78d07a6b8bbad9701b293e49
andrew <159058655+afterddark@users.noreply.github.com>
2025-08-27 10:09:53 -0500

    Revert

--- src/lib/tabs.ts
+++ src/lib/tabs.ts
@@ -87,7 +87,7 @@ document.addEventListener("astro:page-load", () => {
           localStorage.getItem("engine") ||
           "https://www.google.com",
       );
-  setTimeout(() => TitleBar(), 1);
+  setTimeout(() => TitleBar(), 40000);
 
   if (fullscreen) {
     fullscreen.addEventListener("click", () => {
