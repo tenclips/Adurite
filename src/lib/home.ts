53454a2bef0ddfebd8caa5aaefb43f0f29072b86
andrew <159058655+afterddark@users.noreply.github.com>
2025-08-28 10:23:07 -0500

    Add splash text

--- src/lib/home.ts
+++ src/lib/home.ts
@@ -23,3 +23,31 @@ document.addEventListener("astro:page-load", () => {
     });
   }
 });
+
+// splash text
+// feel free to customize
+const splashes = [
+  "how are you today?",
+  "I'm fine?",
+  "go check out the games",
+  "Fast and secure!",
+  "youtube ai is stupid",
+  "use invidious instead",
+  "Made with <3 by afterddark",
+  "Start using Tailwind frfr",
+  "Kanye is mid",
+  "woah",
+  "i want a taco rn",
+  "6 7 is dead",
+  "ipad kid detected",
+  "Over 300 games!",
+  "Check out official links on the Github repo",
+  "I'm thinking about renaming this"
+]
+
+const selection = Math.floor(Math.random() * splashes.length)
+const splashText = splashes[selection]
+
+const splashTextElement = document.getElementById("splashText") as HTMLElement | null
+
+splashTextElement.textContent = splashText;
